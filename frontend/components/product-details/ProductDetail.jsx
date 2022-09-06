187746ee50e4522ba34a3bde37437919904b6510
SachinCoder1 <sachinhlo232@gmail.com>
2022-09-06 13:01:43 +0530

    fixing product detail page

--- frontend/components/product-details/ProductDetail.jsx
+++ frontend/components/product-details/ProductDetail.jsx
@@ -1,8 +1,9 @@
 import { Button, Input, Typography } from "@material-tailwind/react";
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { AiOutlineArrowRight } from "react-icons/ai";
 import { FaEthereum } from "react-icons/fa";
 import { IPFS_URL } from "../../constants";
+import { MainContext } from "../../context/MainContext";
 import Modal from "../../subcomponents/modal/Modal";
 
 export default function ProductDetail({
@@ -17,6 +18,7 @@ export default function ProductDetail({
   setOpen,
   children,
 }) {
+  const { isAdmin } = useContext(MainContext);
   const [parsedMetaData, setParsedMetaData] = useState({});
   useEffect(() => {
     const fetchMetaData = async () => {
@@ -48,42 +50,44 @@ export default function ProductDetail({
                 <p className="text-gray-600">{parsedMetaData?.description}</p>
                 <p className="text-gray-600 md:text-3xl text-xl gap-x-1 flex items-center">
                   Total Quantity Available : {quantity} Units
-                  <span>
-                    <Modal
-                      open={open}
-                      setOpen={setOpen}
-                      text={parsedMetaData.title}
-                    >
-                      <div className="flex w-full text-center flex-col space-y-5">
-                        <Typography
-                          variant="lead"
-                          color="black"
-                          className="text-center"
-                        >
-                          Current Quantity : {quantity}
-                        </Typography>
+                  {isAdmin() && (
+                    <span>
+                      <Modal
+                        open={open}
+                        setOpen={setOpen}
+                        text={parsedMetaData.title}
+                      >
+                        <div className="flex w-full text-center flex-col space-y-5">
+                          <Typography
+                            variant="lead"
+                            color="black"
+                            className="text-center"
+                          >
+                            Current Quantity : {quantity}
+                          </Typography>
 
-                        <Input
-                          name="quantity"
-                          type="number"
-                          value={quantityInput}
-                          onChange={(e) => {
-                            setQuantityInput(e.target.value);
-                          }}
-                          label="Enter New Quantity"
-                        />
-                        <Button
-                          onClick={() => updateProduct()}
-                          className="flex items-center justify-center text-base gap-x-2 bg-primary"
-                          fullWidth
-                          disabled={!quantityInput.length}
-                        >
-                          Update Now
-                          <AiOutlineArrowRight className="text-2xl" />
-                        </Button>
-                      </div>
-                    </Modal>
-                  </span>
+                          <Input
+                            name="quantity"
+                            type="number"
+                            value={quantityInput}
+                            onChange={(e) => {
+                              setQuantityInput(e.target.value);
+                            }}
+                            label="Enter New Quantity"
+                          />
+                          <Button
+                            onClick={() => updateProduct()}
+                            className="flex items-center justify-center text-base gap-x-2 bg-primary"
+                            fullWidth
+                            disabled={!quantityInput.length}
+                          >
+                            Update Now
+                            <AiOutlineArrowRight className="text-2xl" />
+                          </Button>
+                        </div>
+                      </Modal>
+                    </span>
+                  )}
                 </p>
                 <p className="mb-5 font-bold md:text-4xl text-3xl text-primary flex items-center gap-x-1.5">
                   <span className="text-gray-600 text-xl">Price :</span>
